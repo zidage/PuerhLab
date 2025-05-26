d2f78757631ff924c3e344215b31dd5c14cd4c7d
Yurun Zi <yzi2@wisc.edu>
2025-05-26 21:33:32 +0800

    feat(sleeve mapper): add support for EXIF exporting for image files

--- pu-erh_lab/tests/storage/sleeve_mapper_test.cpp
+++ pu-erh_lab/tests/storage/sleeve_mapper_test.cpp
@@ -1,6 +1,7 @@
 #include "storage/mapper/sleeve/sleeve_mapper.hpp"
 #include "sleeve/sleeve_manager.hpp"
 
+#include <exiv2/error.hpp>
 #include <gtest/gtest.h>
 
 #include <exception>
@@ -8,6 +9,7 @@
 #include <stdexcept>
 
 using namespace puerhlab;
+
 
 std::filesystem::path db_path("D:\\Projects\\pu-erh_lab\\pu-erh_lab\\tests\\resources\\temp_folder\\test.db");
 TEST(SleeveMapperTest, DISABLED_InitTest1) {
@@ -47,11 +49,12 @@ TEST(SleeveMapperTest, DISABLED_SimpleCaptureTest1) {
 
 TEST(SleeveMapperTest, SimpleCaptureTest2) {
   {
+    Exiv2::LogMsg::setLevel(Exiv2::LogMsg::Level::mute);
     SleeveMapper  mapper{db_path};
     SleeveManager manager{};
     image_path_t  path =
         L"D:\\Projects\\pu-erh_lab\\pu-erh_"
-        L"lab\\tests\\resources\\sample_images\\jpg";
+        L"lab\\tests\\resources\\sample_images\\raw_nikon";
     std::vector<image_path_t> imgs;
     for (const auto &img : std::filesystem::directory_iterator(path)) {
       imgs.push_back(img.path());
