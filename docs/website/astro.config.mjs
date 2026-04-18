846df63807d43806e9d87ba257f322f6d279ee4c
Yurun Zi <yzi2@wisc.edu>
2026-04-18 00:24:09 -0500

    fix: Update base path to AlcedoStudio in configuration

--- docs/website/astro.config.mjs
+++ docs/website/astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://zidage.github.io',
-  base: '/PuerhLab/',
+  base: '/AlcedoStudio/',
   trailingSlash: 'always',
   integrations: [tailwind()],
   i18n: {
